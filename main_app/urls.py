060e67ce848e89c5bd8707106017a83d4b4f8991
ChristinaGASF <hughes.christina.m@gmail.com>
2019-01-02 05:57:00 -0800

    initial commit

---
+++ main_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib import admin
+from main_app import views
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('admin/', admin.site.urls),
+  path('<int:cat_id>/', views.show, name='show'),
+  path('post_url/', views.post_cat, name='post_cat'),
+  path('user/<username>', views.profile, name='profile'),
+  path('login/', views.login_view, name='login'),
+  path('logout/', views.logout_view, name='logout'),
+  path('like_cat/', views.like_cat, name='like_cat')
+]
+
